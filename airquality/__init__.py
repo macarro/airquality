efa52abfad0d714d2d50e733d9510ca536be2943
macarro <migmackle@alum.us.es>
2021-06-06 12:56:46 +0200

    Added timeseries endpoint

--- airquality/__init__.py
+++ airquality/__init__.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 sql_api_url = 'https://aasuero.carto.com:443/api/v2/sql'
 measurement_variables = ['so2', 'no2', 'co', 'o3', 'pm10', 'pm2_5']
 statistical_measurements = ['avg', 'max', 'min', 'sum', 'count']
+steps = ['hour', 'day', 'week']
 
 # Measurements endpoint
 @app.route('/measurements', methods=['GET'])
@@ -43,6 +44,52 @@ def measurements(args):
     WHERE m.timeinstant >= '{args['from']}'
     AND m.timeinstant < '{args['to']}'
     GROUP BY s.station_id, s.the_geom, g.population
+    """
+    response = requests.get(
+        url=sql_api_url,
+        params={
+            'q': query
+        }
+    )
+    return response.json()
+
+# Timeseries endpoint
+@app.route('/timeseries', methods=['GET'])
+@use_args(
+    {
+        'variable': fields.Str(
+            required=True,
+            validate=validate.OneOf(measurement_variables)
+        ),
+        'measurement': fields.Str(
+            required=True,
+            validate=validate.OneOf(statistical_measurements)
+        ),
+        'from': fields.DateTime(
+            required=True
+        ),
+        'to': fields.DateTime(
+            required=True
+        ),
+        'step': fields.Str(
+            required=True,
+            validate=validate.OneOf(steps)
+        )
+    },
+    location='query')
+def timeseries(args):
+    query = f"""
+    SELECT s.station_id, g.population,
+    {args['measurement']}(m.{args['variable']}) as {args['measurement']}_{args['variable']},
+    date_trunc('{args['step']}', timeinstant) as interval_start
+    FROM aasuero.test_airquality_stations s
+    JOIN aasuero.test_airquality_measurements m
+    ON s.station_id=m.station_id
+    JOIN aasuero.esp_grid_1km_demographics g
+    ON ST_Contains(g.the_geom, s.the_geom)
+    WHERE m.timeinstant >= '{args['from']}'
+    AND m.timeinstant < '{args['to']}'
+    GROUP BY s.station_id, s.the_geom, g.population, interval_start
     """
     response = requests.get(
         url=sql_api_url,
---
+++ tests/test_timeseries.py
@@ -0,0 +1,173 @@
+import pytest
+import airquality
+import json
+
+@pytest.fixture
+def client():
+    with airquality.app.test_client() as client:
+        yield client
+
+def test_many_responses(client):
+    """A valid request should return a rows object"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 200
+    assert 'rows' in body
+    assert 'population' in body['rows'][0]
+
+def test_future_date(client):
+    """A request for date in the future should return no rows"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': '2050-06-01T00:00:00',
+        'to': '2050-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 200
+    assert 'rows' in body
+    assert body['total_rows'] == 0
+
+def test_invalid_variable(client):
+    """A request with an invalid variable value should return an error for that parameter"""
+    params = {
+        'variable': 'invalid',
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'variable' in body['errors']['query']
+
+def test_missing_variable(client):
+    """A request without variable value should return an error for that parameter"""
+    params = {
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'variable' in body['errors']['query']
+
+def test_invalid_measurement(client):
+    """A request with an invalid measurement value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'invalid',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'measurement' in body['errors']['query']
+
+def test_missing_measurement(client):
+    """A request without measurement value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'measurement' in body['errors']['query']
+
+def test_invalid_from(client):
+    """A request with an invalid from value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': 'invalid',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'from' in body['errors']['query']
+
+def test_missing_from(client):
+    """A request without from value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'to': '2017-07-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'from' in body['errors']['query']
+
+def test_invalid_to(client):
+    """A request with an invalid to value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'to': 'invalid',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'to' in body['errors']['query']
+
+def test_missing_to(client):
+    """A request without to value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'step': 'day'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'to' in body['errors']['query']
+
+def test_invalid_step(client):
+    """A request with an invalid step value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00',
+        'step': 'invalid'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'step' in body['errors']['query']
+
+def test_missing_step(client):
+    """A request without step value should return an error for that parameter"""
+    params = {
+        'variable': 'so2',
+        'measurement': 'avg',
+        'from': '2017-06-01T00:00:00',
+        'to': '2017-07-01T00:00:00'
+    }
+    response = client.get('/timeseries', query_string=params)
+    body = json.loads(response.data)
+    assert response.status_code == 422
+    assert 'step' in body['errors']['query']
